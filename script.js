a45b933770626026f9d50c5d8baf11f224a1ac4b
Ziyad <115634322+Ziyad21z@users.noreply.github.com>
2023-11-20 10:04:45 +0300

    fixed a ui bug and added basic functions

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstNumber;
+let secondNumber;
+let operater;
+const screen = document.querySelector('.screen')
+document.querySelector('.clear').addEventListener('click', () => screen.textContent = '')
+
+function sum(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+};
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate() {
+// will do later ;)
+}
