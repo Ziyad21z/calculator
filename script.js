c52455a9bd2198e7a8de01f91a3e7882257eaafa
Ziyad <115634322+Ziyad21z@users.noreply.github.com>
2023-11-22 13:10:21 +0300

    11/22  1:10PM
    fixed some bugs and mad the calculator calculate and display result but it still needs some work and there are still a few bugs

--- script.js
+++ script.js
@@ -1,25 +1,75 @@
 let firstNumber;
 let secondNumber;
 let operater;
+let result = 0;
+
 const screen = document.querySelector('.screen')
-document.querySelector('.clear').addEventListener('click', () => screen.textContent = '')
+const upperScreen = document.querySelector('.upperScreen')
+const lowerScreen = document.querySelector('.lowerScreen')
 
-function sum(a, b) {
-    return a + b
-}
+document.querySelector('.clear').addEventListener('click', () => {
+    upperScreen.textContent = ''
+    lowerScreen.textContent = ''
+    result = 0;
+    firstNumber = 0;
+    secondNumber = 0;
+})
 
-function subtract(a, b) {
-    return a - b
-}
+document.querySelector('.sum').addEventListener('click', () => operater = '+')
+document.querySelector('.subtract').addEventListener('click', () => operater = '-')
+document.querySelector('.multiply').addEventListener('click', () => operater = '*')
+document.querySelector('.divide').addEventListener('click', () => operater = '/')
+document.querySelector('.operate').addEventListener('click', () => {
+    operate(firstNumber, secondNumber, operater)
+})
 
-function multiply(a, b) {
-    return a * b
-};
-
-function divide(a, b) {
-    return a / b
+function display(item) {
+    upperScreen.innerText += item
 }
 
 function operate() {
-// will do later ;)
+    let text = upperScreen.textContent;
+    let splitedtext = text.split(/[-|+|x|÷]+/);
+    firstNumber = splitedtext[0]
+    secondNumber = splitedtext[1]
+    switch (operater) {
+        case '+':
+            result = sum(firstNumber, secondNumber)
+            break;
+        case '/':
+            result = divide(firstNumber, secondNumber)
+            break;
+        case '*':
+            result = multiply(firstNumber, secondNumber)
+            break;
+        case '-':
+            result = subtract(firstNumber, secondNumber)
+            break;
+    }
+    firstNumber = result;
+    lowerScreen.innerText = result
+}
+
+function sum(a, b) {
+    let convertedA = parseInt(a)
+    let convertedB = parseInt(b)
+    return convertedA + convertedB
+}
+
+function subtract(a, b) {
+    let convertedA = parseInt(a)
+    let convertedB = parseInt(b)
+    return convertedA - convertedB
+}
+
+function multiply(a, b) {
+    let convertedA = parseInt(a)
+    let convertedB = parseInt(b)
+    return convertedA * convertedB
+};
+
+function divide(a, b) {
+    let convertedA = parseInt(a)
+    let convertedB = parseInt(b)
+    return convertedA / convertedB
 }
